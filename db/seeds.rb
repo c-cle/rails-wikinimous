151e7745fbe47c744c2e339061a47dc75c36dbbb
Claudio Clemente <claudioclemente@hotmail.it>
2022-05-24 16:26:07 +0200

    Rails Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  title = Faker::Books::Dune.character
+  content = Faker::Books::Dune.quote
+  Article.create(title: title, content: content)
+end
